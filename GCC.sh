288fc2cc284097b589ba9851bf816c7d5acb8d2e
Kuklev Pavel <lgk1144@gmail.com>
2025-08-23 20:10:39 +0300

    unit tests

--- GCC.sh
+++ GCC.sh
@@ -4,5 +4,5 @@ g++ -o MIPT MIPT.cpp -lm -D _DEBUG -ggdb3  -O0 -Wall -Wextra \
     -Wformat-nonliteral -Wformat-security -Wformat-signedness \
      -Wformat=2 -Winline -Wlogical-op -Wopenmp-simd  -Wpacked -Wpointer-arith -Winit-self -Wredundant-decls -Wshadow -Wsign-conversion -Wstrict-overflow=2 -Wsuggest-attribute=noreturn -Wsuggest-final-methods -Wsuggest-final-types -Wswitch-default -Wswitch-enum -Wsync-nand -Wundef -Wunreachable-code -Wunused -Wuseless-cast -Wvariadic-macros -Wno-missing-field-initializers -Wno-narrowing -Wno-varargs -Wstack-protector -fcheck-new -fstack-protector -fstrict-overflow -flto-odr-type-merging -fno-omit-frame-pointer -Wlarger-than=8192 -Wstack-usage=8192 -pie -fPIE -Werror=vla -fsanitize=address,alignment,bool,bounds,enum,float-cast-overflow,float-divide-by-zero,integer-divide-by-zero,leak,nonnull-attribute,null,object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,undefined,unreachable,vla-bound,vptr
 
-./MIPT  #"/home/pasha/p/coefficients.txt"
+./MIPT  #"/home/pasha/p/PROJECT/coefficients.txt"
 
--- MIPT.cpp
+++ MIPT.cpp
@@ -2,51 +2,38 @@
 #include <math.h>
 #include <assert.h>
 #define dx   1e-5
-#define NearZero(x) (fabs((x)) <= dx)
-
-// TODO: assert \/
-// unittests \/
-// x(x+2)
-// square lineal
-
-
-const int    INF = -1;
-
-struct equation{
-    double a = 0, b = 0 ,c = 0;
-};
+#define IsZero(x) (fabs((x)) <= dx)
 
 int test_solver(void);
+
+int print_sol(int num_sol, double x1, double x2);
+
 double checkRoot(struct equation P, double x);
 
 int RootsChecker(struct equation P, double x1, double x2);
 
 int solver(struct equation P, double * x1, double * x2);
 
-int print_sol(int num_sol, double x1, double x2);
+int linearSolver(struct equation P, double * x1);
+
+int quadraticSolver(struct equation P, double * x1 , double * x2);
+
+int ReadFromFile(const char * argv, struct equation P, double * x1 , double * x2);
+===
+const int INF = -1;
+
+struct equation{
+    double a = 0, b = 0 ,c = 0;
+};
 
 int main(int argc, const char * argv[]){
     test_solver();
     struct equation P = {};
     double x1 = 0, x2 = 0;
     int    num_sol = 0;
-
     if (argc > 1){
-
-        FILE * fin = fopen(argv[1], "r");
-        if(fin == NULL){
-            perror("a");
-            return 1;
-        }
-        //while(1){
-        //
-        //}
-        fscanf(fin, "%lf%lf%lf", &P.a, &P.b, &P.c);
-        num_sol = solver(P, &x1, &x2);
-        print_sol(num_sol, x1, x2);
-        return 0;
+        ReadFromFile(argv[1], P, &x1, &x2);
     }
-
     while(1){
         printf("Enter the coefficients of the quadratic equation:\n");
         int scanOK = scanf("%lf%lf%lf", &P.a, &P.b, &P.c);
@@ -54,7 +41,7 @@ int main(int argc, const char * argv[]){
             printf("ERROR: Incorrect input \n\n");
             break;
         }
-        num_sol=solver(P, &x1, &x2);
+        num_sol = solver(P, &x1, &x2);
         print_sol(num_sol, x1, x2);
     }
 
@@ -62,42 +49,33 @@ int main(int argc, const char * argv[]){
 }
 
 int solver(struct equation P, double * x1, double * x2){
-    assert(x1!=NULL);
-    assert(x2!=NULL);
-    if (NearZero(P.a)){
-        if (NearZero(P.b)){
-            if(NearZero(P.c)){
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    if (IsZero(P.a)){
+        if (IsZero(P.b)){
+            if(IsZero(P.c)){
                 return INF;
             }
             return 0;
         }
-        *x1 = (-P.c) / P.b;
-        return 1;
+        return linearSolver(P, x1);
     }
     else{
-        double D = P.b * P.b - 4 * P.a * P.c;
-        if (D>0){
-            *x1 = (-P.b + sqrt(D)) / (2 * P.a);
-            *x2 = (-P.b - sqrt(D)) / (2 * P.a);
-            return 2;
+        if (IsZero(P.c)){
+            *x1=0;
+            return 1+linearSolver(P, x2);
         }
-        else if (NearZero(D)){
-            *x1 = (-P.b) / (2 * P.a);
-            return 1;
-        }
-        else{
-            return 0;
-        }
+        return quadraticSolver(P, x1, x2);
     }
 }
 
-int print_sol(int num_sol, double x1,double x2){
+int print_sol(int num_sol, double x1, double x2){
     switch(num_sol){
     case 2:
-        printf("Equation has 2 solutions: x1=%lg x2=%lg\n\n",x1,x2);
+        printf("Equation has 2 solutions: x1 = %lg x2 = %lg\n\n",x1,x2);
         break;
     case 1:
-        printf("Equation has 1 solution: x=%lg\n\n",x1);
+        printf("Equation has 1 solution: x = %lg\n\n",x1);
         break;
     case 0:
         printf("Equation hasn't solutions\n\n");
@@ -112,25 +90,87 @@ int print_sol(int num_sol, double x1,double x2){
 }
 
 int test_solver(void){
-    struct equation P = {1, -5, 6};
-    double x1 = 0, x2 = 0;
-    int num_sol = solver(P, &x1, &x2);
-    if(!(num_sol==2 && RootsChecker(P,x1,x2)==1)){
-        printf("FALED: (should be x1=3 x2=2) RESULT: x1=%lg x2=%lg\n\n",x1,x2);
-    }
-    else{
-        printf("SUCCESS: (should be x1=3 x2=2) RESULT: x1=%lg x2=%lg\n\n",x1,x2);
+    FILE * fin = fopen("/home/pasha/p/PROJECT/Unit_Tests.txt", "r");
+    struct equation P;
+    int i = 0;
+    while(1){
+        double x1 = 0, x2 = 0;
+        double true_x1 = 0, true_x2 = 0;
+        int scanOK=fscanf(fin, "%lf%lf%lf%lf%lf", &P.a, &P.b, &P.c, &true_x1, &true_x2);
+        if(scanOK != 5){
+            break;
+        }
+        solver(P, &x1, &x2);
+        if(!(IsZero(true_x1-x1) && IsZero(true_x2-x2))){
+            printf("FALED: TEST %d (should be x1 = %lg x2 = %lg) RESULT: x1 = %lg x2 = %lg\n\n", i, true_x1, true_x2, x1, x2);
+        }
+        else{
+            printf("SUCCESS: TEST %d (should be x1 = %lg x2 = %lg) RESULT: x1 = %lg x2 = %lg\n\n" , i, true_x1, true_x2, x1, x2);
+        }
+        i++;
     }
     return 0;
 }
+/*
 int RootsChecker(struct equation P, double x1, double x2){
-    if (NearZero(checkRoot(P,x1)) && (NearZero(checkRoot(P,x2)))){
+    if (IsZero(checkRoot(P,x1)) && (IsZero(checkRoot(P,x2)))){
         return 1;
     }
     else{
         return 0;
     }
 }
+
 double checkRoot(struct equation P, double x){
     return P.a * x * x + P.b * x +P.c ;
 }
+*/////
+
+
+int linearSolver(struct equation P, double * x){
+    assert(x != NULL);
+    *x = (-P.c) / P.b;
+    return 0;
+}
+
+int quadraticSolver(struct equation P, double * x1 , double * x2){
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    double D = P.b * P.b - 4 * P.a * P.c;
+    if (D>0){
+        *x1 = (-P.b + sqrt(D)) / (2 * P.a);
+        *x2 = (-P.b - sqrt(D)) / (2 * P.a);
+        return 2;
+    }
+    else if (IsZero(D)){
+        *x1 = (-P.b) / (2 * P.a);
+        return 1;
+    }
+    else{
+        return 0;
+    }
+
+}
+
+int ReadFromFile(const char * argv, struct equation P, double * x1 , double * x2){
+    assert(x1 != NULL);
+    assert(x2 != NULL);
+    FILE * fin = fopen(argv, "r");
+    if(fin == NULL){
+        perror("a");
+        return 1;
+    }
+    int num_sol = 0;
+    while(1){
+        int scanOK=fscanf(fin, "%lf%lf%lf", &P.a, &P.b, &P.c);
+        if(scanOK != 3){
+            printf("ERROR: Incorrect input \n\n");
+            break;
+        }
+        num_sol = solver(P, x1, x2);
+        print_sol(num_sol, *x1, *x2);
+    }
+    return 0;
+}
+
+
